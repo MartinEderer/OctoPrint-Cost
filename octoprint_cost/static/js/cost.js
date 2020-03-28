ddb57c6292c15f842d464fe32ec00babffcba553
Martin Ederer <martin.ederer@freenet.de>
2020-03-28 19:09:34 +0100

    Update cost.js

--- octoprint_cost/static/js/cost.js
+++ octoprint_cost/static/js/cost.js
@@ -42,16 +42,32 @@ $(function() {
             var cost_per_time = settingsState.settings.plugins.cost.cost_per_time();
 
             var filament_used_length = printerState.filament()[0].data().length / 1000;
-            var filament_used_volume = printerState.filament()[0].data().volume / 1000;
-            var expected_time = (printerState.printTime() + printerState.printTimeLeft()) / 3600;
-
+            var radius = (1.75 / 2);
+            var filament_used_volume = (printerState.filament()[0].data().length) * radius * radius * Math.PI;
+            //var expected_time = (printerState.printTime() + printerState.printTimeLeft()) / 3600;
+            var expected_time1 = printerState.estimatedPrintTime();
+            var expected_time2 = printerState.lastPrintTime();
+            if(expected_time2 === null)
+            {
+                var expected_time = expected_time1 /3600;
+                var zusatz = "(geschaetzt)";
+            }
+            else
+            {
+                var expected_time = expected_time2 /3600;
+                var zusatz = "(genau)";
+            } 
             if (settingsState.check_cost()) {
-                var totalCost = cost_per_weight * filament_used_volume * density_of_filament + expected_time * cost_per_time;
+                //var totalCost = cost_per_weight * filament_used_volume * density_of_filament + expected_time * cost_per_time;
+                var totalCostMaterial = filament_used_volume * density_of_filament * cost_per_weight / 1000 / 1000;
+                var totalCostElectricity = expected_time * cost_per_time;
+                var totalCost = totalCostMaterial + totalCostElectricity;
+
             } else {
                 var totalCost = cost_per_length * filament_used_length + expected_time * cost_per_time;
             }
 
-            return '' + currency + totalCost.toFixed(2);
+            return '' + totalCost.toFixed(2) + currency + zusatz + '(Material: '+ totalCostMaterial.toFixed(2) + currency + ' Strom: ' +totalCostElectricity.toFixed(2)+currency+')';
         });
 
         var originalGetAdditionalData = filesState.getAdditionalData;
@@ -67,7 +83,9 @@ $(function() {
                     var density_of_filament = settingsState.settings.plugins.cost.density_of_filament();
                     var cost_per_time = settingsState.settings.plugins.cost.cost_per_time();
                     var filament_used_length = gcode.filament.tool0.length / 1000;
-                    var filament_used_volume = gcode.filament.tool0.volume / 1000;
+                    //var filament_used_volume = gcode.filament.tool0.volume / 1000;
+                    var radius = (1.75 / 2);
+                    var filament_used_volume = gcode.filament.tool0.length * radius * radius * Math.PI;
 
                     // Use last print time instead of estimation for prints that are already printed at least once
                     if (data["prints"] && data["prints"]["last"] && data["prints"]["last"]["printTime"]) {
@@ -77,16 +95,26 @@ $(function() {
                     }
 
                     if (settingsState.check_cost()) {
-                        var totalCost = cost_per_weight * filament_used_volume * density_of_filament + expected_time * cost_per_time;
+                        var totalCostMaterial = filament_used_volume * density_of_filament * cost_per_weight / 1000 / 1000;
+                        var totalCostElectricity = expected_time * cost_per_time;
+                        var totalCost = totalCostMaterial + totalCostElectricity;
+
+                       // var totalCost = cost_per_weight * filament_used_volume * density_of_filament + expected_time * cost_per_time;
                     } else {
                         var totalCost = cost_per_length * filament_used_length + expected_time * cost_per_time;
                     }
 
                     // Build different output string for first time prints (the octoprint frontend adds a new line or not depending on this. A bug?)
                     if (data["prints"] && data["prints"]["last"] && data["prints"]["last"]["printTime"]) {
-                        output += "<br>" + gettext("Cost") + ": " + currency + totalCost.toFixed(2);
+                        output += "<br>Material (genau): "  + totalCostMaterial.toFixed(2) + currency;
+                        output += "<br>Strom (genau): "  + totalCostElectricity.toFixed(2) + currency;
+                        output += "<br>Kosten (genau): " + totalCost.toFixed(2) + currency;
                     } else {
-                        output += gettext("Cost") + ": " + currency + totalCost.toFixed(2);
+                        //output += gettext("Cost") + " (estimation): " + currency + totalCost.toFixed(2);
+
+                        output += "Material (genau): "  + totalCostMaterial.toFixed(2) + currency;
+                        output += "<br>Strom (gesch&auml;tzt): "  + totalCostElectricity.toFixed(2) + currency;
+                        output += "<br>Kosten (gesch&auml;tzt): " + totalCost.toFixed(2) + currency;
                     }
                 }
             }
@@ -97,7 +125,7 @@ $(function() {
         self.onStartup = function() {
             var element = $("#state").find(".accordion-inner .progress");
             if (element.length) {
-                var text = gettext("Cost");
+                var text = "Kosten";
                 element.before(text + ": <strong data-bind='text: costString'></strong><br>");
             }
         };
